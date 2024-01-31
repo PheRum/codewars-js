a3fa14a65ea32ea5d0989245cdd36322ce952b4e
PheRum <pherum83@gmail.com>
2024-01-31 21:59:06 +0300

    Example task (#1)
    * wip
    
    * wip
    
    * wip

---
+++ 8kyu/_example/task.test.js
@@ -0,0 +1,5 @@
+describe('example test', () => {
+    it('1+1=2', () => {
+        expect(sum(1, 1)).toBe(2);
+    });
+});
--- 8kyu/example/task.test.js
+++
@@ -1,9 +0,0 @@
-describe('example test', () => {
-    it('1+1=2', () => {
-        expect(sum(1, 1)).toEqual(2);
-    });
-
-    it('16+16=21', () => {
-        expect(sum(16, 16)).toEqual(32);
-    });
-});
