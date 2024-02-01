5a1dbce022162f05d001545efa0a474a1c326f89
Valery Malyshev <pherum83@gmail.com>
2024-02-01 18:36:40 +0300

    Filter out the geese

---
+++ 8kyu/Filter out the geese/task.test.js
@@ -0,0 +1,13 @@
+describe('Basic tests', function () {
+    it('Mixed list', function () {
+        expect(gooseFilter(['Mallard', 'Hook Bill', 'African', 'Crested', 'Pilgrim', 'Toulouse', 'Blue Swedish'])).
+        toEqual(['Mallard', 'Hook Bill', 'Crested', 'Blue Swedish']);
+    });
+    it('No geese', function () {
+        expect(gooseFilter(['Mallard', 'Barbary', 'Hook Bill', 'Blue Swedish', 'Crested'])).
+        toEqual(['Mallard', 'Barbary', 'Hook Bill', 'Blue Swedish', 'Crested']);
+    });
+    it('All geese', function () {
+        expect(gooseFilter(['African', 'Roman Tufted', 'Toulouse', 'Pilgrim', 'Steinbacher'])).toEqual([]);
+    });
+});
