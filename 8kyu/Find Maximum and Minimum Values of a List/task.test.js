71d0d103f5c839d07320d53b5c40be2e1a1ac46f
Valery Malyshev <pherum83@gmail.com>
2024-02-01 20:28:23 +0300

    Find Maximum and Minimum Values of a List

---
+++ 8kyu/Find Maximum and Minimum Values of a List/task.test.js
@@ -0,0 +1,11 @@
+describe('Tests', () => {
+    it('min', () => {
+        expect(min([-52, 56, 30, 29, -54, 0, -110])).toBe(-110);
+        expect(min([42, 54, 65, 87, 0])).toBe(0);
+    });
+
+    it('max', () => {
+        expect(max([4, 6, 2, 1, 9, 63, -134, 566])).toBe(566);
+        expect(max([5])).toBe(5);
+    });
+});
